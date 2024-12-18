17d205c6d4fe9db33d8839440a1657185438ca65
Antony Merle <antony.merle@gmail.com>
2024-12-18 13:44:19 +0100

    Enhance Home component with French instructions and improve layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 import { FileUpload } from "../components/FileUpload";
 import { Dashboard } from "../components/Dashboard";
 import { processCSV } from "../utils/processData";
@@ -24,11 +25,62 @@ export default function Home() {
 
   return (
     <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">
+      <h1 className="text-3xl font-semibold mb-6 text-gray-800">
         Tableau de bord des fréquentations générées par affluences
       </h1>
-      {!statistics && <FileUpload onFileUpload={handleFileUpload} />}
-      {statistics && <Dashboard statistics={statistics} />}
+      <p className="mb-4 text-gray-600">
+        Mode d'emploi pour obtenir la visualisation de données depuis le{" "}
+        <a
+          className="text-blue-600 hover:underline"
+          href="https://admin.affluences.com/"
+          target="_blank"
+        >
+          portail d'administration d'Affluences
+        </a>
+      </p>
+      <ul className="pl-4 text-gray-700">
+        <li>Aller dans Statistiques/Historique</li>
+        <li className="flex space-x-4">
+          <p>Vérifiez que la granularité temporelle est sur demi-heure</p>
+          <Image
+            alt="granularité temporelle"
+            src="temporel.jpg"
+            width={568}
+            height={98}
+            className="max-w-40"
+          />
+        </li>
+        <li className="flex space-x-4">
+          <p>
+            Dans "Fréquentation" vérifier que le type de données cochées soient
+            uniquement "Entrées" et "Sorties"
+          </p>
+          <Image
+            alt="type de données"
+            src="donnees.jpg"
+            width={490}
+            height={228}
+            className="max-w-40"
+          />
+        </li>
+        <li className="flex space-x-4">
+          <p>
+            Choisir la période voulue (il est possible de sélectionner plusieurs
+            années)
+          </p>
+          <Image
+            alt="période"
+            src="periode.jpg"
+            width={524}
+            height={138}
+            className="max-w-40"
+          />
+        </li>
+      </ul>
+      <div className="py-12">
+        {!statistics && <FileUpload onFileUpload={handleFileUpload} />}
+        {statistics && <Dashboard statistics={statistics} />}
+      </div>
     </main>
   );
 }
